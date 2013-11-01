b000e1a91d98b760f5c2315159562bbe11145ecb
bigmonkey <conway@xerpi.com>
2013-11-01 15:26:30 -0400

    gemfile, config set up, base code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hold::Application.config.secret_key_base = '51efd69b75bd077e95e51095795bc8b29afe511022c96b1d6c726823b72e1c7b7199e3a74b72801fe439e15465d26c44f14279f5101291b8d81ab36f566f8d64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hold::Application.config.session_store :cookie_store, key: '_hold_session'
